eaeb31d2e51fca969f3f37243a1de49de32cc01f
Taylor Chen <s68175@gmail.com>
2020-07-23 23:02:34 +0800

    Modify index.hbs

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
+const Todo = require('./models/todo') // 載入 Todo model
 
 const app = express()
 
@@ -25,8 +26,12 @@ app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
+
 
 // 設定 port 3000
 app.listen(3000, () => {
