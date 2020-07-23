f2696157aa242369a8776b4c0a93fe8651339596
Taylor Chen <s68175@gmail.com>
2020-07-23 21:58:34 +0800

    Add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars');
 
 const app = express()
 
@@ -13,14 +14,18 @@ const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
 })
+
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
