eb16cac86ee5a86c4274644f824dabec81c581e5
Taylor Chen <s68175@gmail.com>
2020-07-28 01:09:56 +0800

    Modify mongoose config

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-
 // 載入 method-override
 const methodOverride = require('method-override')
 
-const Todo = require('./models/todo') // 載入 Todo model
-
 // 引用路由器
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
