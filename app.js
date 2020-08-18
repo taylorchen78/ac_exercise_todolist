7da3b88721255b5f2e21d3fd675154d5b4cc33e7
Taylor Chen <s68175@gmail.com>
2020-08-18 23:57:44 +0800

    Add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 // 載入 method-override
@@ -15,6 +16,13 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
